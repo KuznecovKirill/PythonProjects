5f47a0d7789bd14747842a8bfc70be9257eaf596
Kirill <kirillkuznecov03@mail.ru>
2022-11-17 17:29:20 +0500

    Создание ряда b из случайных чисел и вывод их sum

--- Laba1/laba1Numpy.py
+++ Laba1/laba1Numpy.py
@@ -3,3 +3,5 @@ import random
 a = np.arange(0.0,9999999.0)
 print("Сумма ряда",np.sum(a))
 print("Среднее значение",np.mean(a))
+b = np.random.randint(0,100,10000)
+print("Сумма ряда случайных чисел",np.sum(b))
