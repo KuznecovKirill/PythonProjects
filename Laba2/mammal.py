7138f8accf1e7261e4747f6e775b352e2f3ee404
Kirill <kirillkuznecov03@mail.ru>
2022-11-29 09:35:39 +0500

    Класс Млекопитающий

---
+++ Laba2/mammal.py
@@ -0,0 +1,7 @@
+import json
+class Mammal:
+    def __init__(self, name = "",age = 0,gender = "", view =""):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.view = view
