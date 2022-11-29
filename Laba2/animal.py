6f602881a693d6d751a78c0781063b4c2345dcab
Kirill <kirillkuznecov03@mail.ru>
2022-11-29 09:35:14 +0500

    Метод _str_

--- Laba2/animal.py
+++ Laba2/animal.py
@@ -1,3 +1,5 @@
+import json
+from mammal import Mammal
 class Animal:
     def __init__(self, name = "", age = 0, gender = ""):
         self.name = name
@@ -5,3 +7,5 @@ class Animal:
         self.gender = gender
     def __repr__(self): # отображение объекта в режиме отладки
         return self.__str__()
+    def __str__(self):
+        return str(self.__dict__)
