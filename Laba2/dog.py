7f9f2e98d48c41d76c961e81481bdd4b9537172a
Kirill <kirillkuznecov03@mail.ru>
2022-11-29 10:56:52 +0500

    Вывод Dog

--- Laba2/dog.py
+++ Laba2/dog.py
@@ -1,9 +1,11 @@
 import json
 from mammal import Mammal
 class Dog(Mammal):
-    def __init__(self, name = "", age = 0, gender = "", view = "", viewDog = ""):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.view = view
+    def __init__(self, name = "", age = 0, gender = "", viewDog = ""):
+        super().__init__(name,age,gender,"Собака")
         self.viewDog = viewDog
+    def __repr__(self)->str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return str(self.__dict__)
+    
--- Laba2/main.py
+++ Laba2/main.py
@@ -6,6 +6,8 @@ from dog import Dog
 
 newAnimal = Animal("Джастин",7,"М")
 newMammal = Mammal("Лео",4,"М","Обезьяна")
+newDog = Dog("Жанна",7,"Ж","Пудель")
 print(newAnimal)
 print(newMammal)
+print(newDog)
 AnimalSerialize(newAnimal,"primer.json")
