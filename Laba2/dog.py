167747ef71f4249740c3adb292e69c924b517e20
Kirill <kirillkuznecov03@mail.ru>
2022-11-30 15:07:04 +0500

    Сериализация/десериализация Dog в primer3.json

--- Laba2/dog.py
+++ Laba2/dog.py
@@ -5,8 +5,19 @@ class Dog(Mammal):
     def __init__(self, name = "", age = 0, gender = "", viewDog = ""):
         super().__init__(name,age,gender,"Собака")
         self.viewDog = viewDog
-    def __repr__(self)->str:
+    def __repr__(self): 
         return self.__str__()
-    def __str__(self) -> str:
+    def __str__(self):
         return str(self.__dict__)
-    
+
+
+def DogSerialize(dog,path):
+    with open(path,"w") as outfile:
+        json.dump(dog.__dict__,outfile)
+def DogDeserialize(pas):
+    def Decode(obj):
+        if "viewDog" in obj:
+            return Dog(obj["name"],obj["age"],obj["gender"],obj["viewDog"])
+    with open(pas) as json_file:
+        J = json.load(json_file)
+    return Decode(J) 
--- Laba2/main.py
+++ Laba2/main.py
@@ -2,7 +2,7 @@ import json
 from pprint import pprint
 from animal import Animal, AnimalSerialize, AnimalDeserialize
 from mammal import Mammal, MammalSerialize, MammalDeserialize
-from dog import Dog
+from dog import Dog, DogSerialize, DogDeserialize
 
 newAnimal = Animal("Джастин",7,"М")
 newMammal = Mammal("Лео", 4, "М", "Обезьяна")
@@ -10,11 +10,17 @@ newDog = Dog("Жанна",7,"Ж","Пудель")
 print(newAnimal)
 print(newMammal)
 print(newDog)
-
+#Сериализация/десериализация Animal
 AnimalSerialize(newAnimal,"primer.json")
 newAnimal2 = AnimalDeserialize("primer.json")
 pprint(newAnimal2.__dict__)
-
+#Сериализация/десериализация Mammal
 MammalSerialize(newMammal,"primer2.json")
 newMammal2 = MammalDeserialize("primer2.json")
 pprint(newMammal2.__dict__)
+#Сериализация/десериализация Dog
+DogSerialize(newDog,"primer3.json")
+newDog2 = DogDeserialize("primer3.json")
+pprint(newDog2.__dict__)
+
+
