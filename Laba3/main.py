00db841336add84b104025d1e0a588502fbae472
Kirill <kirillkuznecov03@mail.ru>
2022-12-02 07:53:10 +0500

    Сумма ряда

--- Laba3/main.py
+++ Laba3/main.py
@@ -1,5 +1,9 @@
-from pyDatalog import pyDatalog
-
+from pyDatalog import pyDatalog 
 #Посчитать сумму ряда [0, 9999999]
-pyDatalog.create_terms("arrSum, n, N")
 
+pyDatalog.create_terms('arrSum, N')
+
+arrSum[N] = N + arrSum[N-1]
+arrSum[0] = 1
+
+print(arrSum[9999999]==N)
