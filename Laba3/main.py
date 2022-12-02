6575403eb9ff67c6c4fa5ce5ac5bf77b4e74ca1f
Kirill <kirillkuznecov03@mail.ru>
2022-12-02 08:06:45 +0500

    Медиана ряда случайных чисел

--- Laba3/main.py
+++ Laba3/main.py
@@ -2,7 +2,7 @@ from pyDatalog import pyDatalog
 import random
 #Посчитать сумму ряда 
 
-pyDatalog.create_terms('arrSum, N, AVS, arrRandom')
+pyDatalog.create_terms('arrSum, N, AVS, arrRandom, arrRandom2, arrRandomMedian')
 
 arrSum[N] = N + arrSum[N-1]
 arrSum[0] = 1
@@ -17,3 +17,11 @@ print(AVS[200]==N)
 arrRandom[N] = random.randint(0,100) + arrRandom[N - 1]
 arrRandom[1] = random.randint(0, 100)
 print(arrRandom[100]==N)
+
+#Медиана ряда случайных чисел
+arrRandom2[N] = random.randint(0,100) + arrRandom2[N - 1]
+arrRandom2[1] = random.randint(0, 100)
+arrRandomMedian[N] = arrRandom2[N] / N
+print(arrRandomMedian[100] == N)
+
+
