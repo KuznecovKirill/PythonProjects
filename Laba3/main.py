3967908f03d8a96992576bffe7ee5e4ee811bb2f
Kirill <kirillkuznecov03@mail.ru>
2022-12-02 08:00:44 +0500

    Сумма ряда случайных чисел

--- Laba3/main.py
+++ Laba3/main.py
@@ -1,7 +1,8 @@
 from pyDatalog import pyDatalog 
+import random
 #Посчитать сумму ряда 
 
-pyDatalog.create_terms('arrSum, N, AVS')
+pyDatalog.create_terms('arrSum, N, AVS, arrRandom')
 
 arrSum[N] = N + arrSum[N-1]
 arrSum[0] = 1
@@ -11,3 +12,8 @@ print(arrSum[200]==N)
 # Среднее значение ряда
 AVS[N] = (N + 1)/2
 print(AVS[200]==N)
+
+# Сумма ряда случайных чисел [1,100]
+arrRandom[N] = random.randint(0,100) + arrRandom[N - 1]
+arrRandom[1] = random.randint(0, 100)
+print(arrRandom[100]==N)
