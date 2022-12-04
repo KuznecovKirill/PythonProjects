f5bd7b9ef696352c5fa8d45156ef1b8ba5583716
Kirill <kirillkuznecov03@mail.ru>
2022-12-04 13:56:54 +0500

    Реализация Reduce

--- Laba4/MapAndReduce.py
+++ Laba4/MapAndReduce.py
@@ -3,3 +3,12 @@ def MyMap(func, originalList: list) -> list: #Реализация Map
     for object in originalList:
         newList.append(func(object))
     return newList
+
+def MyReduce(func, originalList: list): #Реализация Reduce
+    i: int = 2;
+    length: int = len(originalList);
+    result = func(originalList[0], originalList[1]);
+    while i < length:
+        result = func(result, originalList[i]);
+        i += 1;
+    return result;
